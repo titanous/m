de9392dcaf3374230376cf7c272dc965365ad23c
Jonathan Rudenberg <jonathan@titanous.com>
2012-11-01 11:25:00 -0400

    Refactor to store column data

--- mapper.go
+++ mapper.go
@@ -12,36 +12,76 @@ import (
 type Mapping struct {
 	DB *sql.DB
 
-	tables map[reflect.Type]string
+	tables map[reflect.Type]tableMap
+}
+
+type tableMap struct {
+	Name    string
+	Type    reflect.Type
+	Columns []columnMap
+}
+
+type columnMap struct {
+	Name       string
+	Serialize  bool
+	PrimaryKey bool
+	Field      int
 }
 
 func NewMapping() *Mapping {
-	return &Mapping{tables: make(map[reflect.Type]string)}
+	return &Mapping{tables: make(map[reflect.Type]tableMap)}
 }
 
 // AddTable adds a table to struct mapping to a Mapping.
 //	M.AddTable("posts", Post{})
 func (m *Mapping) AddTable(name string, thing interface{}) {
 	typ := reflect.TypeOf(thing)
-	m.tables[typ] = name
+	m.tables[typ] = tableMap{name, typ, getTableColumns(thing, typ)}
+}
+
+func getTableColumns(thing interface{}, typ reflect.Type) (columns []columnMap) {
+	fieldCount := typ.NumField()
+
+	for i := 0; i < fieldCount; i++ {
+		field := typ.Field(i)
+		tag := strings.Split(field.Tag.Get("db"), ",")
+		if len(tag) > 0 {
+			col := columnMap{Field: i}
+			for _, flag := range tag {
+				switch flag {
+				case "pk":
+					col.PrimaryKey = true
+				case "serialize":
+					col.Serialize = true
+				default:
+					if col.Name == "" {
+						col.Name = flag
+					}
+				}
+			}
+			columns = append(columns, col)
+		}
+	}
+
+	return columns
 }
 
 // Insert takes a struct and inserts it into the appropriate table.
 // If a field is nil it will not be part of the INSERT statement.
 func (m *Mapping) Insert(thing interface{}) error {
-	typ, name := m.lookupTable(thing)
-	columns, values := prepareInsertSqlColumnsValues(thing, typ)
-	_, err := m.DB.Exec(sqlInsertString(name, columns), values...)
+	table := m.lookupTable(thing)
+	columns, values := prepareInsertSqlColumnsValues(thing, table)
+	_, err := m.DB.Exec(sqlInsertString(table.Name, columns), values...)
 	return err
 }
 
 // Update takes a struct and a map of column names to data and updates the struct and the database row.
 func (m *Mapping) Update(thing interface{}, data map[string]interface{}) error {
-	typ, name := m.lookupTable(thing)
-	columns, values := updateAndGetSqlColumnsValues(thing, typ, data)
-	key_columns, key_values := keysForUpdate(thing, typ)
+	table := m.lookupTable(thing)
+	columns, values := updateAndGetSqlColumnsValues(thing, table, data)
+	key_columns, key_values := keysForUpdate(thing, table)
 	values = append(values, key_values...)
-	_, err := m.DB.Exec(sqlUpdateString(name, columns, key_columns), values...)
+	_, err := m.DB.Exec(sqlUpdateString(table.Name, columns, key_columns), values...)
 	return err
 }
 
@@ -76,44 +116,37 @@ func (m *Mapping) doSelect(thing interface{}, query string, bindings ...interfac
 		return nil, err
 	}
 
-	typ := reflect.TypeOf(thing)
+	table := m.lookupTable(thing)
 	results := make([]interface{}, 0)
 
 	for rows.Next() {
-		instance := reflect.New(typ)
+		instance := reflect.New(table.Type)
 		values := make([]interface{}, len(columns))
 		deserializeValues := make(map[int]interface{})
 
 		for x := range columns {
-			var fieldName string
-			var serialize bool
+			var column columnMap
 			columnName := columns[x]
-			fieldCount := typ.NumField()
 
-			for i := 0; i < fieldCount; i++ {
-				field := typ.Field(i)
-				if field.Tag.Get("db") == columnName {
-					fieldName = field.Name
-					if field.Tag.Get("serialize") == "true" {
-						serialize = true
-					}
+			for i := 0; i < len(table.Columns); i++ {
+				if table.Columns[i].Name == columnName {
+					column = table.Columns[i]
 					break
 				}
 			}
 
-			field := instance.Elem().FieldByName(fieldName)
-
-			if !field.IsValid() {
-				return nil, fmt.Errorf("m: No field `%s` in type %s (query: `%s`)", columnName, typ.Name(), query)
+			if column.Name == "" {
+				return nil, fmt.Errorf("m: No field `%s` in type %s (query: `%s`)", columnName, table.Type.Name(), query)
 			}
 
-			if serialize {
+			field := instance.Elem().Field(column.Field)
+
+			if column.Serialize {
 				values[x] = new([]byte)
 				deserializeValues[x] = field.Addr().Interface()
 			} else {
 				values[x] = field.Addr().Interface()
 			}
-
 		}
 
 		err = rows.Scan(values...)
@@ -137,49 +170,45 @@ func (m *Mapping) doSelect(thing interface{}, query string, bindings ...interfac
 	return results, nil
 }
 
-func (m *Mapping) lookupTable(thing interface{}) (reflect.Type, string) {
+func (m *Mapping) lookupTable(thing interface{}) tableMap {
 	typ := tableType(thing)
-	if name, ok := m.tables[typ]; ok {
-		return typ, name
+	if table, ok := m.tables[typ]; ok {
+		return table
 	}
 
 	panic(fmt.Sprintf("Unknown table for type: %v (%v)", tableType, typ.Kind()))
 }
 
 func tableType(thing interface{}) reflect.Type {
-	thingPointer := reflect.ValueOf(thing)
-	if thingPointer.Kind() != reflect.Ptr {
-		panic(fmt.Sprintf("Expecting pointer, got %v (%v)", thing, thingPointer.Kind()))
+	thingVal := reflect.Indirect(reflect.ValueOf(thing))
+	if thingVal.Kind() != reflect.Struct {
+		panic(fmt.Sprintf("Expecting struct or struct pointer, got %v (%v)", thing, thingVal.Kind()))
 	}
-	return reflect.TypeOf(thingPointer.Elem().Interface())
+	return thingVal.Type()
 }
 
-func prepareInsertSqlColumnsValues(thing interface{}, thingType reflect.Type) (columns []string, values []interface{}) {
+func prepareInsertSqlColumnsValues(thing interface{}, table tableMap) (columns []string, values []interface{}) {
 	thingValue := reflect.Indirect(reflect.ValueOf(thing))
-	numFields := thingType.NumField()
 
-	for i := 0; i < numFields; i++ {
-		field := thingType.Field(i)
-		column := field.Tag.Get("db")
-		serialize := field.Tag.Get("serialize")
-		value := thingValue.Field(i)
+	for i := 0; i < len(table.Columns); i++ {
+		column := table.Columns[i]
+		value := thingValue.Field(column.Field)
 		kind := value.Kind()
 
-		// skip fields that aren't tagged with a column name, nil pointers, empty slices/maps/arrays
-		if column == "" ||
-			(kind == reflect.Ptr && value.IsNil()) ||
+		// skip fields that are nil pointers or empty slices/maps/arrays
+		if (kind == reflect.Ptr && value.IsNil()) ||
 			((kind == reflect.Slice || kind == reflect.Map || kind == reflect.Array) && value.Len() < 1) {
 			continue
 		}
 
-		if serialize != "" {
+		if column.Serialize {
 			// TODO(jr): don't eat this marshal error value
 			marshaled, _ := json.Marshal(value.Interface())
 			values = append(values, string(marshaled))
 		} else {
 			values = append(values, reflect.Indirect(value).Interface())
 		}
-		columns = append(columns, column)
+		columns = append(columns, column.Name)
 	}
 
 	return columns, values
@@ -191,49 +220,46 @@ func sqlInsertString(tableName string, columns []string) string {
 	return fmt.Sprintf("INSERT INTO %s (%s) VALUES (%s)", tableName, columnsStr, valuesStr)
 }
 
-func updateAndGetSqlColumnsValues(thing interface{}, thingType reflect.Type, data map[string]interface{}) (columns []string, values []interface{}) {
+func updateAndGetSqlColumnsValues(thing interface{}, table tableMap, data map[string]interface{}) (columns []string, values []interface{}) {
 	thingValue := reflect.Indirect(reflect.ValueOf(thing))
-	numFields := thingType.NumField()
 
-	for i := 0; i < numFields; i++ {
-		field := thingType.Field(i)
-		column := field.Tag.Get("db")
-		if val, ok := data[column]; ok {
-			destField := thingValue.Field(i)
-			serialize := field.Tag.Get("serialize")
+	for i := 0; i < len(table.Columns); i++ {
+		column := table.Columns[i]
+
+		if val, ok := data[column.Name]; ok {
+			destField := thingValue.Field(column.Field)
 			value := reflect.ValueOf(val)
 
 			// assign the value from the data map to the destination struct field
 			destField.Set(value)
 
-			if serialize != "" {
+			if column.Serialize {
 				// TODO(jr): don't eat this marshal error value
 				marshaled, _ := json.Marshal(val)
 				values = append(values, string(marshaled))
 			} else {
 				values = append(values, reflect.Indirect(value).Interface())
 			}
-			columns = append(columns, column)
+			columns = append(columns, column.Name)
 		}
 	}
 
 	return columns, values
 }
 
-func keysForUpdate(thing interface{}, thingType reflect.Type) (columns []string, values []interface{}) {
+func keysForUpdate(thing interface{}, table tableMap) (columns []string, values []interface{}) {
 	thingValue := reflect.Indirect(reflect.ValueOf(thing))
-	numFields := thingType.NumField()
 
-	for i := 0; i < numFields; i++ {
-		field := thingType.Field(i)
-		if field.Tag.Get("pk") == "" {
+	for i := 0; i < len(table.Columns); i++ {
+		column := table.Columns[i]
+
+		if !column.PrimaryKey {
 			continue
 		}
 
-		column := field.Tag.Get("db")
-		value := thingValue.Field(i)
+		value := thingValue.Field(column.Field)
 
-		columns = append(columns, column)
+		columns = append(columns, column.Name)
 		values = append(values, reflect.Indirect(value).Interface())
 	}
 
