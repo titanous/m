a40a5c2e3583045eb19b2c0f060a045ff072c436
Jonathan Rudenberg <jonathan@titanous.com>
2012-12-07 20:17:24 -0500

    Move functions to tableMap

--- mapper.go
+++ mapper.go
@@ -32,6 +32,7 @@ type tableMap struct {
 	Name    string
 	Type    reflect.Type
 	Columns []columnMap
+	m       *Mapping
 }
 
 type columnMap struct {
@@ -45,7 +46,7 @@ type columnMap struct {
 //	M.AddTable("posts", Post{})
 func (m *Mapping) AddTable(name string, thing interface{}) {
 	typ := reflect.TypeOf(thing)
-	m.tables[typ] = tableMap{name, typ, getTableColumns(thing, typ)}
+	m.tables[typ] = tableMap{name, typ, getTableColumns(thing, typ), m}
 }
 
 func getTableColumns(thing interface{}, typ reflect.Type) []columnMap {
@@ -78,10 +79,7 @@ func getTableColumns(thing interface{}, typ reflect.Type) []columnMap {
 // Insert takes a struct and inserts it into the appropriate table.
 // If a field is nil it will not be part of the INSERT statement.
 func (m *Mapping) Insert(thing interface{}) error {
-	table := m.lookupTable(thing)
-	columns, values := prepareInsertSqlColumnsValues(thing, table)
-	_, err := m.DB.Exec(sqlInsertString(table.Name, columns, m.Type), values...)
-	return err
+	return m.lookupTable(thing).insert(thing)
 }
 
 func (m *Mapping) InsertValues(table string, columns []string, values ...interface{}) error {
@@ -91,23 +89,18 @@ func (m *Mapping) InsertValues(table string, columns []string, values ...interfa
 
 // Update takes a struct and a map of column names to data and updates the struct and the database row.
 func (m *Mapping) Update(thing interface{}, data map[string]interface{}) error {
-	table := m.lookupTable(thing)
-	columns, values := updateAndGetSqlColumnsValues(thing, table, data)
-	key_columns, key_values := keysForUpdate(thing, table)
-	values = append(values, key_values...)
-	_, err := m.DB.Exec(sqlUpdateString(table.Name, columns, key_columns, m.Type), values...)
-	return err
+	return m.lookupTable(thing).update(thing, data)
 }
 
 // Select queries the database and returns a slice containing the returned rows scanned into structs with 
 // the same type as thing.
 func (m *Mapping) Select(thing interface{}, query string, bindings ...interface{}) ([]interface{}, error) {
-	return m.doSelect(thing, query, bindings...)
+	return m.lookupTable(thing).doSelect(query, bindings...)
 }
 
 // SelectOne is a convenience function that returns a single record or nil if no record is found.
 func (m *Mapping) SelectOne(thing interface{}, query string, bindings ...interface{}) (interface{}, error) {
-	res, err := m.doSelect(thing, query, bindings...)
+	res, err := m.lookupTable(thing).doSelect(query, bindings...)
 	if err == nil && len(res) < 1 {
 		return nil, nil
 	}
@@ -117,9 +110,23 @@ func (m *Mapping) SelectOne(thing interface{}, query string, bindings ...interfa
 	return res[0], nil
 }
 
+func (t tableMap) insert(thing interface{}) error {
+	columns, values := prepareInsertSqlColumnsValues(thing, t)
+	_, err := t.m.DB.Exec(sqlInsertString(t.Name, columns, t.m.Type), values...)
+	return err
+}
+
+func (t tableMap) update(thing interface{}, data map[string]interface{}) error {
+	columns, values := updateAndGetSqlColumnsValues(thing, t, data)
+	keyColumns, keyValues := keysForUpdate(thing, t)
+	values = append(values, keyValues...)
+	_, err := t.m.DB.Exec(sqlUpdateString(t.Name, columns, keyColumns, t.m.Type), values...)
+	return err
+}
+
 // Mostly taken from https://github.com/coopernurse/gorp by James Cooper
-func (m *Mapping) doSelect(thing interface{}, query string, bindings ...interface{}) ([]interface{}, error) {
-	rows, err := m.DB.Query(query, bindings...)
+func (t tableMap) doSelect(query string, bindings ...interface{}) ([]interface{}, error) {
+	rows, err := t.m.DB.Query(query, bindings...)
 	if err != nil {
 		return nil, err
 	}
@@ -130,11 +137,10 @@ func (m *Mapping) doSelect(thing interface{}, query string, bindings ...interfac
 		return nil, err
 	}
 
-	table := m.lookupTable(thing)
 	results := make([]interface{}, 0)
 
 	for rows.Next() {
-		instance := reflect.New(table.Type)
+		instance := reflect.New(t.Type)
 		values := make([]interface{}, len(columns))
 		deserializeValues := make(map[int]interface{})
 
@@ -142,15 +148,15 @@ func (m *Mapping) doSelect(thing interface{}, query string, bindings ...interfac
 			var column columnMap
 			columnName := columns[x]
 
-			for i := 0; i < len(table.Columns); i++ {
-				if table.Columns[i].Name == columnName {
-					column = table.Columns[i]
+			for i := 0; i < len(t.Columns); i++ {
+				if t.Columns[i].Name == columnName {
+					column = t.Columns[i]
 					break
 				}
 			}
 
 			if column.Name == "" {
-				return nil, fmt.Errorf("m: No field `%s` in type %s (query: `%s`)", columnName, table.Type.Name(), query)
+				return nil, fmt.Errorf("m: No field `%s` in type %s (query: `%s`)", columnName, t.Type.Name(), query)
 			}
 
 			field := instance.Elem().Field(column.Field)
